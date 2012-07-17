a52d732123f726f15ef51eb4d8366b0de54458fa
cristianpark <cristianpark@gmail.com>
2012-07-17 15:29:04 -0500

    Se suben carpetas proyecto Batalla Naval

---
+++ src/batallanaval/com/co/Login.java
@@ -0,0 +1,24 @@
+package batallanaval.com.co;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v4.app.NavUtils;
+
+public class Login extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_login, menu);
+        return true;
+    }
+
+    
+}
