6072f7e3c314273323357b6df702427deeea8516
crixguitarr <cristi1053799766>
2012-07-18 21:09:20 -0500

    clases batalla naval

--- src/batallanaval/com/co/Login.java
+++ src/batallanaval/com/co/Login.java
@@ -1,15 +1,22 @@
 package batallanaval.com.co;
 
+import batallanaval.com.co.clases.tablero;
+import batallanaval.com.co.clases.tablero.Posicion;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
 public class Login extends Activity {
-
+	tablero tab;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        tab=new tablero();
+        Posicion pos = tab.movimiento(1, 2);
+        System.out.print(pos);
+        
+        
     }
 
     @Override
@@ -17,6 +24,6 @@ public class Login extends Activity {
         getMenuInflater().inflate(R.menu.activity_login, menu);
         return true;
     }
-
+    
     
 }
